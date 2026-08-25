00b6f8d81e8695290c7d208c36cae725671a02e6
Verg-2 <kadiraltundag145@gmail.com>
2026-08-25 16:30:41 +0300

    Update API base URL to point to Azure

--- vue-proje/src/services/api.js
+++ vue-proje/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://profilsite.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://kadir-api.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json'
   },
